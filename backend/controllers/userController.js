c06449892d590a894bf39511b8f88039e69d2827
Alvin Chou <alvinchou516@hotmail.com>
2022-11-24 12:51:09 -0500

    added to getMe

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -76,8 +76,13 @@ const loginUser = asyncHandler(async (req, res) => {
 // @route   GET api/users/me (get the current user info by sending the the token and getting th id from the token  )
 // @access  Private
 const getMe = asyncHandler(async (req, res) => {
-    res.json({message: "User data display"})
+    const { _id, name, email} = await User.findById(req.user.id)
 
+    res.status(200).json({
+        id: _id,
+        name,
+        email
+    })
 })
 
 // Generate JWT
