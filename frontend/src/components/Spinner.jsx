c6230697dd3932c74c659407d2d483617d965ea0
Alvin Chou <alvinchou516@hotmail.com>
2022-11-24 21:38:50 -0500

    added logout function

---
+++ frontend/src/components/Spinner.jsx
@@ -0,0 +1,9 @@
+function Spinner () {
+    return (
+        <div className="loadingSpinnerContainer">
+            <div className="loadingSPinn"></div>
+        </div>
+    )
+}
+
+export default Spinner
--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -1,22 +1,27 @@
 // this is for making the http request and storing the information in local storage
 
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = '/api/user/'
+const API_URL = "/api/users/";
 
 // register user
 const register = async (userData) => {
-    const response = await axios.post(API_URL, userData)
+    const response = await axios.post(API_URL, userData);
 
     if (response.data) {
-        localStorage.setItem('user', JSON.stringify(response.data))
+        localStorage.setItem("user", JSON.stringify(response.data));
     }
 
-    return response.data
-}
+    return response.data;
+};
+
+// logout user
+const logout = () => {
+    localStorage.removeItem("user");
+};
 
 const authService = {
-    register
-}
+    register, logout
+};
 
-export default authService
+export default authService;
