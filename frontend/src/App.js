d0145e71f67e5ca08799da1afc04a9c379752302
Alvin Chou <alvinchou516@hotmail.com>
2022-11-24 21:52:47 -0500

    fixed toastify css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import { ToastContainer } from "react-toastify";
-// import 'react-toastify/dist/ReactToasify.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
