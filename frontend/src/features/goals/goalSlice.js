19daf276920cbefb64d6985a79d298b55fe6d941
Alvin Chou <alvinchou516@hotmail.com>
2022-11-28 12:16:30 -0500

    fetch goals from server and display on dashboard

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,20 +1,22 @@
-import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-import goalService from './goalService'
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import goalService from "./goalService";
 
 const initialState = {
     goals: [],
     isError: false,
     isSuccess: false,
     isLoading: false,
-    message: ''
-}
+    message: "",
+};
 
 // create new goal
-export const createGoal = createAsyncThunk('goals/create', async (goalData, thunkAPI) => {
-    try {
-        const token = thunkAPI.getState().auth.user.token
-        return await goalService.createGoal(goalData, token)
-    } catch (error) {
+export const createGoal = createAsyncThunk(
+    "goals/create",
+    async (goalData, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token;
+            return await goalService.createGoal(goalData, token);
+        } catch (error) {
             const message =
                 (error.response &&
                     error.response.data &&
@@ -23,32 +25,65 @@ export const createGoal = createAsyncThunk('goals/create', async (goalData, thun
                 error.toString();
             return thunkAPI.rejectWithValue(message);
         }
-})
+    }
+);
+
+// get user goals
+export const getGoals = createAsyncThunk(
+    "goals/getAll",
+    async (_, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token;
+            return await goalService.getGoals(token);
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
 
 // create the slice
 export const goalSlice = createSlice({
-    name: 'goal',
+    name: "goal",
     initialState,
     reducers: {
-        reset: (state) => initialState
+        reset: (state) => initialState,
     },
     extraReducers: (builder) => {
         builder
             .addCase(createGoal.pending, (state) => {
-                state.isLoading = true
+                state.isLoading = true;
             })
             .addCase(createGoal.fulfilled, (state, action) => {
-                state.isLoading = false
-                state.isSuccess = true
-                state.goals.push(action.payload)
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.goals.push(action.payload);
             })
             .addCase(createGoal.rejected, (state, action) => {
-                state.isLoading = false
-                state.isError = true
-                state.message = action.payload
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
             })
-    }
-})
+            .addCase(getGoals.pending, (state) => {
+                state.isLoading = true;
+            })
+            .addCase(getGoals.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.isSuccess = true;
+                state.goals = action.payload;
+            })
+            .addCase(getGoals.rejected, (state, action) => {
+                state.isLoading = false;
+                state.isError = true;
+                state.message = action.payload;
+            });
+    },
+});
 
-export const {reset} = goalSlice.actions
-export default goalSlice.reducer
+export const { reset } = goalSlice.actions;
+export default goalSlice.reducer;
